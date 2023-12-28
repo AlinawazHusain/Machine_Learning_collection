4d33419d01a1c3d8b8bb38cdafcbdd888affc2d9
Alinawaz husain <alinawazhusain2002@gmail.com>
2023-12-28 19:46:39 +0530

    Pytorch Models

---
+++ Pytorch/CNN/model.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+
+
+#================================================================================#
+#                         Layers for 64x64 image size                            #
+#================================================================================# 
+
+
+class CNN(nn.Module):
+    def __init__(self , in_ch , num_classes ,features = 16):
+        super(CNN , self).__init__()
+
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_ch , features*8 , 3 , 2 , 1),
+            nn.Conv2d(features*8 , features*4 , 3 , 2 , 1),  
+            nn.Conv2d(features*4 , features*2 , 3 , 2 ,1),  
+            nn.Conv2d(features*2 , features , 3 , 2 ,1),  
+            nn.Conv2d(features , features//2 , 3 , 2 ,1),  
+            nn.Conv2d(features//2 , num_classes , 3 , 2 ,1),  
+        )
+
+
+    def forward(self , x):
+        return self.layers(x)
+    
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    x = torch.randn(3 , 64 , 64)
+    model = CNN(3 , 1 , 16)
+    print(model(x).shape)
---
+++ Pytorch/CNN/train.py
@@ -0,0 +1,88 @@
+import torch
+import torch.optim as optim
+from tqdm import tqdm
+import torch.nn as nn
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import model
+import torchvision.transforms as transform
+import numpy as np
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+learning_rate = 1e-4
+b_size = 64
+epochs = 10
+in_channels = 1  #for MNist for RGB change to 3
+num_classes = 10 #for MNist
+features = 16
+save_model = False
+
+
+Transform = transform.Compose(
+    [
+        transform.Resize((64 , 64)),
+        transform.ToTensor(),
+        transform.Normalize([0.5]*in_channels , [0.5]*in_channels)
+    ]
+)
+
+
+model = model.CNN(in_channels , num_classes , features).to(device)
+optimizer = optim.Adam(model.parameters() , lr = learning_rate )
+loss_fn = nn.CrossEntropyLoss().to(device)
+
+
+train_data = datasets.MNIST(root = 'data/' , train = True , download = True , transform = Transform)
+test_data = datasets.MNIST(root = 'data/' , train = False , transform = Transform, download = True)
+train_loader = DataLoader(train_data , batch_size = b_size , shuffle = True)
+test_loader = DataLoader(test_data , batch_size = b_size , shuffle = True)
+
+
+for epoch in range(epochs):
+    pbar = tqdm(train_loader , desc = f'epoch {epoch}/{epochs}')
+
+    for batch_idx , (image , label ) in enumerate(pbar):
+        image = image.to(device)
+        label = label.to(device)
+
+        y = model(image)
+        y = y.squeeze (-1).squeeze(-1)
+        loss = loss_fn(y , label)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        pbar.set_postfix({'loss ': loss})
+
+
+    if epoch%3 == 0:
+        print("Testing output and accuracy")
+        with torch.no_grad():
+            model.eval()
+            test_pbar = tqdm(test_loader , desc = f'Testing outputs')
+            for _ , (image , label) in enumerate(test_pbar):
+                image = image.to(device)
+                label = label.to(device)
+
+                outputs = model(image).squeeze (-1).squeeze(-1)
+                outputs = torch.argmax(outputs , dim = 1)
+
+                total = outputs.shape[0]
+                accurate = (outputs == label).sum()
+
+                accuracy = accurate/total
+
+            print(f'Accuracy : {accuracy}')
+
+
+
+if save_model:
+    torch.save({
+        'model': model,
+        'state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+    }, 'trained_model.pth.tar')
+
+
