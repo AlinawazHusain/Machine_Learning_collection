37549c834b20d8aaa845948d781d933dec5c4228
Alinawaz Husain <alinawazhusain2002@gmail.com>
2024-06-01 20:59:03 +0530

    RNN added

---
+++ Pytorch/RNN & LSTM/RNN/model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class RNN(nn.Module):
+    def __init__(self , input_size , hidden_size ,num_layers , num_classes  ):
+        super(RNN , self ).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.rnn = nn.RNN(input_size ,hidden_size , num_layers  , batch_first= True)
+        ## RNN can be replace by GRU for better accuracy ##
+        self.linear = nn.Linear(hidden_size*input_size , num_classes)
+
+    def forward(self , x):
+        h0 = torch.zeros(self.num_layers , x.size(0) ,self.hidden_size )
+        out , _ = self.rnn(x , h0)
+        out = out.reshape(out.shape[0] , -1)
+        return self.linear(out)
+
+    
